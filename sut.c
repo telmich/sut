83b2ce34135faa87a4718daac07396b609882def
Ben Kaiser <benjaminjkaiser@gmail.com>
2013-06-02 17:27:31 +1000

    Modified program, not yet working but almost there.

--- sut.c
+++ sut.c
@@ -2,11 +2,28 @@
 #include<stdlib.h>
 #include<time.h>
 
+typedef struct sut {
+    int hour;
+    int min;
+    int sec;
+}SUT_TIME;
+
+// this function turns UDT to STD seconds
+SUT_TIME utc_to_sut(struct timeval time){
+    double seconds = time.tv_sec+(time.tv_usec/1000000.0);;
+    SUT_TIME cur_sut;
+
+    cur_sut.sec = (int)seconds;
+    return cur_sut;
+}
+
 int main(){
-	time_t seconds;
-	seconds = time (NULL);
+	struct timeval tim;
+	gettimeofday(&tim, NULL);
 	
-	printf("Hello World! %ld\n", seconds);
+	SUT_TIME response = utc_to_sut(tim);
+	
+	printf("%d\n", response.sec);
 	
 	return(0);
 }
