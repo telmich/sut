407b73c7d754d4e0866b407d4c22bcd101f30750
Ben Kaiser <benjaminjkaiser@gmail.com>
2013-06-02 20:35:27 +1000

    Getting closer to final solution.

--- sut.c
+++ sut.c
@@ -9,21 +9,38 @@ typedef struct sut {
 }SUT_TIME;
 
 // this function turns UDT to STD seconds
-SUT_TIME utc_to_sut(struct timeval time){
-    double seconds = time.tv_sec+(time.tv_usec/1000000.0);;
-    SUT_TIME cur_sut;
+SUT_TIME seconds_to_sut(int milli){
+	SUT_TIME cur_sut;
 
-    cur_sut.sec = (int)seconds;
+    cur_sut.sec = (int)milli;
     return cur_sut;
 }
 
+// convert a timeval and time to milliseconds of today
+int seconds_of_day(struct timeval sec, struct tm tim){
+    long int milli_since_epoch = (sec.tv_sec * 1000)+(sec.tv_usec/100);
+    long int sec_strt_tdy = 1000 * (sec.tv_sec - 
+    	(tim.tm_sec) - 
+    	(tim.tm_min * 60) - 
+    	(tim.tm_hour * 3600));
+    int tdy_mlsec = (int)(milli_since_epoch - sec_strt_tdy);
+    printf("time in millisec: %ld\n", milli_since_epoch);
+    return tdy_mlsec;
+}
+
 int main(){
-	struct timeval tim;
-	gettimeofday(&tim, NULL);
+	struct timeval *timeval_struct;
+	time_t rawtime;
+	struct tm * tm_struct;
+	gettimeofday(timeval_struct, NULL);
+	time(&rawtime);
+	tm_struct = gmtime(&rawtime);
+
+
+	int milli = seconds_of_day(*timeval_struct, *tm_struct);
+	//SUT_TIME response = utc_to_sut(milli);
 	
-	SUT_TIME response = utc_to_sut(tim);
-	
-	printf("%d\n", response.sec);
+	printf("%d\n", milli);
 	
 	return(0);
 }
